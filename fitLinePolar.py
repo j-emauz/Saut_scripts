f54bc0571d4b5228678cad297983afa763ce6e78
rodrigoavmendes <rodrigo.mendes@tecnico.ulisboa.pt>
2022-06-02 12:04:28 +0100

    cenas

--- fitLinePolar.py
+++ fitLinePolar.py
@@ -1,3 +1,4 @@
+from cmath import pi
 import sys
 
 import math
@@ -6,3 +7,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2 as cv
 
+def fitLinePolar(theta, rho, C_TR):
+    lin_theta, N = theta.shape
+    if lin_theta != 1 or rho.shape[0] != 1
+        print('fitLinePolar only accepts column vectors')
+        exit()
+    if rho.shape[1] != N
+        print('theta and rho must have matching size. But columns_theta == ' + theta.shape[1], end = '')
+        print(", and columns_rho ==" + rho.shape[1])
+        exit()
