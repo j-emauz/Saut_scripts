9a757bb7819971eb7f806d0f11e2d155c5f0b78d
j-emauz <31942182+j-emauz@users.noreply.github.com>
2022-06-03 11:57:53 +0100

    Update fitline.py

--- fitline.py
+++ fitline.py
@@ -23,6 +23,7 @@ def fitline(pontos):
     # a media de cada coordenada
 
     lixo, len = pontos.shape
+    #alpha = np.zeros((1,1))
     
     xc, yc = pontos.sum(axis = 1) / len
     dx = (pontos[0, :] - xc)
@@ -99,14 +100,56 @@ def splitlines(xy, startidx, endidx, thresholds):
     if (splitpos != -1):
         alpha1, r1, idx1 = splitlines(xy, startidx, splitpos+startidx, thresholds) # se calhar start idx-1
         alpha2, r2, idx2 = splitlines(xy, splitpos+startidx, endidx, thresholds)
-        alpha = np.hstack((alpha1, alpha2))
-        r = np.hstack((r1, r2))
-        idx = np.hstack((idx1, idx2))
+        alpha = np.vstack((alpha1, alpha2))
+        r = np.vstack((r1, r2))
+        idx = np.vstack((idx1, idx2))
     else:
-        idx = [startidx, endidx]
+        idx = np.array([startidx, endidx])
 
     return alpha, r, idx
     
+
+def mergeColinear(xy, alpha, r, pointidx, thresholds):
+    z = [alpha[0, 0], r[0, 0]]
+    startidx = pointidx[0, 0]
+    lastendidx = pointidx[0, 1]
+
+    N = r.shape[0]
+    zt = [0, 0]
+
+    rOut = np.zeros((r.shape[0],1))
+    alphaOut = np.zeros((alpha.shape[0], 1))
+    pointidxOut = np.zeros((1, 1))
+
+    j = 0
+
+    for i in range(1, N-1):
+        endidx = pointidx[i,1]
+
+        zt[0], zt[1] = fitline(xy[:, startidx:endidx])
+
+        splitpos = findsplitpos(xy[:, startidx:endidx], zt[0], zt[1], thresholds)
+
+        #Se nao for necessario fazer split, fazemos merge
+        if splitpos == -1:
+            z = zt
+        else: #Sem mais merges
+            alphaOut[j, 0] = z[0]
+            rOut[j, 0] = z[1]
+            pointidxOut[j, :] = [startidx, lastendidx]
+            j = j + 1
+            z = [alpha(i), r(i)]
+            startIdx = pointidx(i, 0)
+
+
+        lastendidx = endidx
+
+    #Adicionar o ultimo segmento
+    alphaOut[j, 0] = z[0]
+    rOut[j, 0] = z[1]
+    pointidxOut[j, :] = [startidx, lastendidx]
+
+    return alphaOut, rOut, pointidxOut
 
 
 
@@ -117,15 +160,19 @@ if __name__ == '__main__':
     thresholds = Thresholds()
 
     alphav, rv, idxv = splitlines(pontos, 0, 4, thresholds)
-    startidx=0
-    endidx=4
+
+    N = rv.shape[0]
+    print(N)
+
+    if N > 1:
+        alphav, rv, idxv = mergeColinear(pontos, alphav, rv, idxv, thresholds)
+
+
 
     # splitpos = findsplitpos(pontos[:, startidx:(endidx + 1)], alpha, r, thresholds)
     print(alphav)
     print(rv)
     print(idxv)
     # print(splitpos)
-    print(alpha)
-    print(r)
     
   
