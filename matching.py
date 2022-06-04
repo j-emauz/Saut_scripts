6b09ed95e08e9badd4260b6084a1fc408f0c767e
aleomr <106688680+aleomr@users.noreply.github.com>
2022-06-04 15:54:38 +0100

    Update matching.py

--- matching.py
+++ matching.py
@@ -1,4 +1,5 @@
 def matching (x, P, Z, R, M, g):
+    #Z: observations measurements
     n_measurs = Z.shape(1)
     n_map = M.shape(1)
 
@@ -8,12 +9,14 @@ def matching (x, P, Z, R, M, g):
 
     for aux_nme in range(0, n_measurs):
         for aux_nmap in range(0, n_map):
-            z_predict, H
+            Z_predict, H[:, :, aux_nmap + (aux_nme -1) * n_map] = measurementfunction(x, M[:, n_map])
+            v[:, aux_nmap + (aux_nme -1) * n_map] = Z[:, aux_nme] - Z_predict
+            W = H[:, :, aux_nmap + (aux_nme -1) * n_map] * P * np.transpose(H[:, :, aux_nmap + (aux_nme -1) * n_map]) + R_seg[:, :, aux_nme]
+            d[aux_nme, aux_nmap] = np.transpose(v[:, aux_nmap + (aux_nme -1) * n_map]) * np.linalg.inv(W) * v[:, aux_nmap + (aux_nme -1) * n_map]
 
+    
 
-
-
-    v = v
-    H =  H
-    R_seg = R_seg(:, :, measursidx)
+    v = v[:, mapidx + (measursidx -1)* n_map]
+    H =  H[:, :,  mapidx + (measursidx -1)* n_map]
+    R_seg = R_seg[:, :, measursidx]
     return v, H, R_seg
