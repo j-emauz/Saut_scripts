388d4a33a0ddf2496d07aba5ee4f5f7c90e9fd49
aleomr <106688680+aleomr@users.noreply.github.com>
2022-06-18 17:07:17 +0100

    yewgyufevfeibefgb

---
+++ experimental_tests.py
@@ -0,0 +1,40 @@
+import sys
+
+from cmath import pi
+import math
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2 as cv
+import scipy.linalg
+
+
+"""
+1ºteste: variar ruído, manter o resto constante
+"""
+#valores constantes: 
+
+valor_medio_xr = np.array([])
+valor_medio_yr = np.array([])
+valor_medio_thetar = np.array([])
+
+
+
+#colocar no powerpoint o valor das covaraincia a dividir pelo numero de testes
+
+
+"""
+2ºteste: variar g
+"""
+
+valor_medio_xg = np.array([])
+valor_medio_yg = np.array([])
+valor_medio_thetag = np.array([])
+
+
+
+"""
+3ºteste: linha-> ver a covariancia da linha , mahalanobis distance ??
+"""
+
+
